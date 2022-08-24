4d950cce764906e2c9f2700f3dbbb06c890c9179
Alade Akeem <aladetomiwa31@gmail.com>
2022-08-24 13:17:12 +0100

    app: created testcases for the chat app.

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
 
 # Create your tests here.
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text='This is the test data for Post')
+
+    def test_model_data(self):
+        self.assertEqual(self.post.text, 'This is the test data for Post')
+
+    def test_url_exist_in_correct_location(self):
+        response = self.client.get('/chat/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        response = self.client.get(reverse('chat'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'This is the test data for Post')
